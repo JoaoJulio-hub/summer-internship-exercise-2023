95e67f4a078d91cd3f84b54e38c46c1a47ed0b55
João Júlio <78657127+JoaoJulio-hub@users.noreply.github.com>
2023-06-11 22:47:53 +0100

    Exercise Finished

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -1,19 +1,25 @@
 package com.premiumminds.internship.snail;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 /**
  * Created by aamado on 05-05-2023.
  */
 class SnailShellPattern implements ISnailShellPattern {
-
+  
   /**
    * Method to get snailshell pattern
    * 
    * @param matrix matrix of numbers to go through
-   * @return order array of values thar represent a snail shell pattern
+   * @return order array of values that represent a snail shell pattern
    */
   public Future<int[]> getSnailShell(int[][] matrix) {
-    throw new RuntimeException("Not Implemented Yet");
+	  ExecutorService executorService = Executors.newFixedThreadPool(1);
+	  Future<int[]> finalResult = executorService.submit(new ComputeSnailShell(matrix));
+	  executorService.shutdown();
+	  return finalResult; 
   };
+  
 }
